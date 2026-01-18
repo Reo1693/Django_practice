2c6dacbe04f972124656bfa5cf4057343788fb0d
Reo1693 <68433786+Reo1693@users.noreply.github.com>
2026-01-18 20:09:45 +0530

    Template data created on templates App

--- templatesDemo/templatesApp/urls.py
+++ templatesDemo/templatesApp/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path
-from templatesApp.views import renderTemplates
+from templatesApp.views import renderTemplates,render_data,render_Employee
 
 urlpatterns = [
     path('temp/',renderTemplates),
+    path('tempdata/',render_data),
+    path('empdata/',render_Employee),
 ]
--- templatesDemo/templatesApp/views.py
+++ templatesDemo/templatesApp/views.py
@@ -4,3 +4,14 @@ from django.shortcuts import render
 
 def renderTemplates(request):
     return render(request,'templatesApp/firstTemplate.html')
+
+def render_data(request):
+    myDict={"name":"Elango"}
+    return render(request,'templatesApp/tempdata.html',context=myDict)
+
+def render_Employee(request):
+    myDict={"id":"007",
+            "name":"Elango",
+            "role":"Software developer",
+            "salary":"18000"}
+    return render(request,'templatesApp/employeedata.html',context=myDict)
